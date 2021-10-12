792dbd367ec0cb76bc400f5cc9d4beff8c6bde31
Shintwl <shinilheo0205@gmail.com>
2021-10-12 18:04:38 +0900

    user에서 article로 변경

--- Devtopia/src/main/java/com/developer/forum/article/model/ArticleVO.java
+++ Devtopia/src/main/java/com/developer/forum/article/model/ArticleVO.java
@@ -1,14 +1,15 @@
 package com.developer.forum.article.model;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class ArticleVO {
-	private String userId;
-	private String userPw;
-	private String userName;
-	private String email;
-	private String nickName;
-	private String phoneNumber;
-	private String address;
+	private int postNo;
+	private String writer;
+	private String title;
+	private String content;
+	private Date writeDate;
+	private int viewCount;
 }
