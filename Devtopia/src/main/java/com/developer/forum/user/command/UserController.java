840400841135c277d55947539357428c9d34d068
Shintwl <shinilheo0205@gmail.com>
2021-10-12 16:51:28 +0900

    requestmapping 추가

--- Devtopia/src/main/java/com/developer/forum/user/command/UserController.java
+++ Devtopia/src/main/java/com/developer/forum/user/command/UserController.java
@@ -2,30 +2,57 @@ package com.developer.forum.user.command;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.developer.forum.user.impl.UserService;
 import com.developer.forum.user.model.UserVO;
 
 @Controller
+@RequestMapping(value = "/user")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	public void insert(UserVO vo) {
+	@RequestMapping(value = "/createUser.do")
+	public String fwdInsertPage() {
+		return "createUser";
+	}
+	
+	@RequestMapping(value = "/createUserAction.do")
+	public String insert(UserVO vo) {
 		userService.insert(vo);
+		return "createSuccess";
 	}
 	
-	public void update(UserVO vo) {
+	@RequestMapping(value = "/modifyUser.do")
+	public String fwdUpdatePage() {
+		return "modifyUser";
+	}
+	
+	@RequestMapping(value = "/modifyUserAction.do")
+	public String update(UserVO vo) {
 		userService.update(vo);
+		return "modifySuccess";
 	}
 	
-	public UserVO select(UserVO vo) {
-		return userService.select(vo);
+	@RequestMapping(value = "/deleteUser.do")
+	public String fwdDeletePage() {
+		return "deleteUser";
 	}
 	
-	public void delete(UserVO vo) {
+	@RequestMapping(value = "/userInfo.do")
+	public String select(UserVO vo, Model model) {
+		UserVO user = userService.select(vo);
+		model.addAttribute("user", user);
+		return "userInfo";
+	}
+	
+	@RequestMapping(value = "/deleteUserAction.do")
+	public String delete(UserVO vo) {
 		userService.delete(vo);
+		return "deleteSucces";
 	}
 	
 }
