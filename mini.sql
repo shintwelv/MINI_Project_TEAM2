65e0a7700c1f04a68aa2dcac3c29aff819bbcc83
Shintwl <shinilheo0205@gmail.com>
2021-10-14 15:45:19 +0900

    users 테이블에 관리자 계정 구분용 admin (값은 Y 또는 N), 프로필 이미지 경로 profileImgLoc (varchar2(100) 추가

--- mini.sql
+++ mini.sql
@@ -14,7 +14,7 @@ CREATE SEQUENCE idx_seq
      INCREMENT BY 1
      CACHE 20;
 
-drop table users;
+drop table users cascade constraints;
 drop table board_news;
 drop table board_free;
 drop table board_tip;
@@ -28,7 +28,9 @@ userName varchar2(15) not null,
 email varchar2(30),
 nickName varchar2(15) not null,
 phoneNumber varchar2(12) not null,
-address varchar2(2000) not null
+address varchar2(2000) not null,
+profileImgLoc varchar2(100),
+admin varchar2(2)
 );
 
 create table board_news(
